4d5061c0ed76ac91cb679ff35dbbf1eff7ccbf48
Devin Carlen <devin.carlen@gmail.com>
2025-04-09 11:42:26 -0700

    Turn off italics in nord theme

--- init.lua
+++ init.lua
@@ -902,7 +902,7 @@ require('lazy').setup({
       vim.g.nord_contrast = true
       vim.g.nord_borders = false
       vim.g.nord_disable_background = false
-      vim.g.nord_italic = true
+      vim.g.nord_italic = false
       vim.g.nord_uniform_diff_background = true
       vim.g.nord_bold = true
 
