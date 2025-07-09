9d1e23eabcbe4e0c7370b8cef903318f44fbf83e
devcamcar <devin.carlen@gmail.com>
2025-07-09 04:26:51 -0700

    Restructured nvim config and added bufferline

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,61 @@
+local opts = { noremap = true, silent = true }
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', ':nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Save file
+vim.keymap.set('n', '<C-s>', ':w<CR>', opts)
+
+-- Close file
+vim.keymap.set('n', '<C-q>', ':q<CR>', opts)
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- Keybinds to make split navigation easier.
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Resize with arrows.
+vim.keymap.set('n', '<Up>', ':resize -2<CR>', opts)
+vim.keymap.set('n', '<Down>', ':resize +2<CR>', opts)
+vim.keymap.set('n', '<Left>', ':vertical resize -2<CR>', opts)
+vim.keymap.set('n', '<Right>', ':vertical resize +2<CR>', opts)
+
+-- Buffers
+--vim.keymap.set('n', '<Tab>', ':bnext<CR>', opts)
+--vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', opts)
+--vim.keymap.set('n', '<leader>x', ':Bdelete!<CR>', opts)
+--vim.keymap.set('n', '<leader>b', ':enew<CR>', opts)
+
+-- Window management
+vim.keymap.set('n', '<leader>v', '<C-w>v', opts)
+vim.keymap.set('n', '<leader>h', '<C-w>s', opts)
+vim.keymap.set('n', '<leader>se', '<C-w>', opts)
+vim.keymap.set('n', '<leader>xs', ':enew<CR>', opts)
+
+-- Tabs
+vim.keymap.set('n', '<C-t>', ':tabnew<CR>', opts)
+vim.keymap.set('n', '<C-w>', ':tabclose<CR>', opts)
+vim.keymap.set('n', '<Tab>', ':tabn<CR>', opts)
+vim.keymap.set('n', '<S-Tab>', ':tabp<CR>', opts)
+
+-- Toggle line wrapping
+vim.keymap.set('n', '<leader>lw', ':set wrap!<CR>', opts)
+
+-- Keep last yanked when pasting
+vim.keymap.set('v', 'p', '"_dP', opts)
---
+++ lua/core/options.lua
@@ -0,0 +1,67 @@
+vim.g.have_nerd_font = true
+vim.opt.mouse = 'a'
+vim.opt.undofile = true
+vim.opt.number = true
+vim.opt.showmode = false
+vim.o.wrap = false
+vim.o.autoindent = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.expandtab = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.o.backspace = 'indent,eol,start'
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = 'yes'
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.opt.confirm = true
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
