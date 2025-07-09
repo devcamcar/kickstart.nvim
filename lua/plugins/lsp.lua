7c211f6a7da9b6d0bc3a652e104f6c055195a973
devcamcar <devin.carlen@gmail.com>
2025-07-09 15:10:51 -0700

    Upgrade mason

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,11 +5,11 @@ return {
     -- Automatically install LSPs and related tools to stdpath for Neovim
     -- Mason must be loaded before its dependents so we need to set it up here.
     -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
-    { 'williamboman/mason.nvim', opts = {} },
-    'williamboman/mason-lspconfig.nvim',
+    { 'mason-org/mason.nvim', opts = {} },
+    'mason-org/mason-lspconfig.nvim',
     'WhoIsSethDaniel/mason-tool-installer.nvim',
     -- Useful status updates for LSP.
-    { 'j-hui/fidget.nvim', opts = {} },
+    { 'j-hui/fidget.nvim',    opts = {} },
     -- Allows extra capabilities provided by nvim-cmp
     'hrsh7th/cmp-nvim-lsp',
   },
@@ -231,7 +231,6 @@ return {
               library = {
                 '${3rd}/luv/library',
                 unpack(vim.api.nvim_get_runtime_file('', true)),
-
               },
             },
             diagnostics = { disable = { 'missing-fields' } },
