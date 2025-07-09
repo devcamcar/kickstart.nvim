6e800c0a9d15040fed9eb24b1dfbd842f0b2dcbd
devcamcar <devin.carlen@gmail.com>
2025-07-09 13:19:38 -0700

    Configure lua ls

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -225,8 +225,16 @@ return {
             completion = {
               callSnippet = 'Replace',
             },
-            -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-            -- diagnostics = { disable = { 'missing-fields' } },
+            runtime = { version = 'LuaJIT' },
+            workspace = {
+              checkThirdParty = false,
+              library = {
+                '${3rd}/luv/library',
+                unpack(vim.api.nvim_get_runtime_file('', true)),
+
+              },
+            },
+            diagnostics = { disable = { 'missing-fields' } },
           },
         },
       },
