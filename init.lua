50ce5d15337ff37a1c16b27a6261008e907ac738
devcamcar <devin.carlen@gmail.com>
2025-08-08 19:11:54 -0700

    Formatting

--- init.lua
+++ init.lua
@@ -54,12 +54,12 @@ require('lazy').setup({
 local notify_original = vim.notify
 vim.notify = function(msg, ...)
   if
-      msg
-      and (
-        msg:match 'position_encoding param is required'
-        or msg:match 'Defaulting to position encoding of the first client'
-        or msg:match 'multiple different client offset_encodings'
-      )
+    msg
+    and (
+      msg:match 'position_encoding param is required'
+      or msg:match 'Defaulting to position encoding of the first client'
+      or msg:match 'multiple different client offset_encodings'
+    )
   then
     return
   end
